d02621e4544e2f8067f7365da4f7e9f30521fbe9
Anthony Lepore <violacellonyc@gmail.com>
2020-07-27 20:27:22 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -8,7 +8,7 @@ else return false
 
 
 def not_safe?(speed)
-	# return speed > 60 || speed < 40 ? true : false
-
+	speed < 40 || speed > 60 ? true : false
+# return speed > 60 || speed < 40 ? true : false
   end
 end
